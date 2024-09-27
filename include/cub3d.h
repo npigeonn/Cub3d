0f1491a42597ae37cf42ee92c00b43aea04f4e30
Yann Beaucousin Negelin <ybeaucou@made-f0Br7s9.clusters.42paris.fr>
2024-09-27 14:01:33 +0200

    d

---
+++ include/cub3d.h
@@ -0,0 +1,11 @@
+
+#ifndef CUB3D_H
+#define CUB3D_H
+
+# include <math.h>
+# include "game.h"
+
+# define MOVEMENT_SPEED 0.1f
+# define ROTATION_SPEED 0.05f
+
+#endif
---
+++ src/render.c
@@ -0,0 +1,126 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybeaucou <ybeaucou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/27 12:58:37 by ybeaucou          #+#    #+#             */
+/*   Updated: 2024/09/27 13:54:23 by ybeaucou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/cub3d.h"
+
+#include "mlx.h"
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define screenWidth 640 * 2
+#define screenHeight 480 * 2
+
+void draw_vertical_line(void *mlx_ptr, void *win_ptr, int x, int start, int end, int color)
+{
+	for (int y = start; y <= end; y++)
+		mlx_pixel_put(mlx_ptr, win_ptr, x, y, color);
+}
+
+int map[24][24] = {
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+};
+
+void raycast(t_player *player, void *mlx_ptr, void *win_ptr)
+{
+	for (int x = 0; x < screenWidth; x++)
+	{
+		double cameraX = 2 * x / (double)screenWidth - 1;
+		double ray_dir_x = player->dx + player->px * cameraX;
+		double ray_dir_y = player->dy + player->py * cameraX;
+
+		int mapX = (int)player->x;
+		int mapY = (int)player->y;
+		
+		double sideDistX;
+		double sideDistY;
+
+		double deltaDistX = fabs(1 / ray_dir_x);
+		double deltaDistY = fabs(1 / ray_dir_y);
+		double perpWallDist;
+
+		int stepX, stepY;
+		int hit = 0;
+		int side;
+		if (ray_dir_x < 0)
+		{
+			stepX = -1;
+			sideDistX = (player->x - mapX) * deltaDistX;
+		}
+		else
+		{
+			stepX = 1;
+			sideDistX = (mapX + 1.0 - player->x) * deltaDistX;
+		}
+		if (ray_dir_y < 0)
+		{
+			stepY = -1;
+			sideDistY = (player->y - mapY) * deltaDistY;
+		} else
+		{
+			stepY = 1;
+			sideDistY = (mapY + 1.0 - player->y) * deltaDistY;
+		}
+		while (hit == 0)
+		{
+			if (sideDistX < sideDistY)
+			{
+				sideDistX += deltaDistX;
+				mapX += stepX;
+				side = 0;
+			}
+			else
+			{
+				sideDistY += deltaDistY;
+				mapY += stepY;
+				side = 1;
+			}
+			if (map[mapX][mapY] > 0) hit = 1;
+		}
+		if (side == 0)
+			perpWallDist = (mapX - player->x + (1 - stepX) / 2) / ray_dir_x;
+		else
+			perpWallDist = (mapY - player->y + (1 - stepY) / 2) / ray_dir_y;
+		int lineHeight = (int)(screenHeight / perpWallDist);
+		int drawStart = -lineHeight / 2 + screenHeight / 2;
+		if (drawStart < 0) drawStart = 0;
+		int drawEnd = lineHeight / 2 + screenHeight / 2;
+		if (drawEnd >= screenHeight) drawEnd = screenHeight - 1;
+		int color = 0xFFFFFF;
+		if (side == 1) color = 0xAAAAAA;
+		draw_vertical_line(mlx_ptr, win_ptr, x, drawStart, drawEnd, color);
+	}
+}
